ad19e93744bd7a11ff3c45da9c86f9d36702ce79
Shep <simss20@mail.wlu.edu>
2021-04-04 16:19:48 -0700

    get line working

--- p5gravity.js
+++ p5gravity.js
@@ -1,29 +1,35 @@
-
-
 function setup() {
-  createCanvas(1000,500);
-  system = new System(mouseX, mouseY);
-  background(51);
-  detached = [];
+    createCanvas(1000,500);
+    system = new System(mouseX, mouseY);
+    background(51);
+    detached = [];
 }
 
 
 function draw() {
-  system.run(mouseX, mouseY);
-  
+    system.run(mouseX, mouseY);
 }
 
 function keyPressed() {
-  if (keyCode == LEFT_ARROW) {
-    system.popParticle();
-  }
-  if (keyCode == RIGHT_ARROW) {
-    system.addParticle();
-  }
-  if (keyCode == DOWN_ARROW) {
-    system.gravity += 0.001;
-  }
-  if (keyCode == UP_ARROW) {
-    system.gravtiy -= 0.001;
-  }
+    if (keyCode == LEFT_ARROW) {
+        system.popParticle();
+    }
+    if (keyCode == RIGHT_ARROW) {
+        system.addParticle();
+    }
+    if (keyCode == DOWN_ARROW) {
+        system.gravity += 0.001;
+    }
+    if (keyCode == UP_ARROW) {
+        system.gravtiy -= 0.001;
+    }
+    if (key == 'l'){
+        system.trace = true;
+    }
+    if (key =='d'){
+        system.trace = false;
+    }
+    if (key =='c'){
+        background(0);
+    }
 }
--- particle.js
+++ particle.js
@@ -3,49 +3,57 @@
 // ----------------------------------------
 
 let Particle = function(system) {
-  this.system = system;
+    this.system = system;
   
-  this.position = createVector(this.system.position.x, this.system.position.y);
-  this.velocity = createVector(0,0);
-  this.acceleration = createVector(0,0);
+    this.position = createVector(this.system.position.x, this.system.position.y);
+    this.velocity = createVector(0,0);
+    this.acceleration = createVector(0,0);
   
-  this.distanceFromSystem = 0;
-  this.angle = 0;
+    this.distanceFromSystem = 0;
+    this.angle = 0;
   
-  this.lifespan = 255;
-  this.mass = 1;
+    this.past = [];
+    this.lifespan = 255;
+    this.mass = 1;
   
 };
 
 Particle.prototype.update = function() {
+    this.color = createVector(0,0,0);
   
-  this.color = createVector(0,0,0);
+    this.distanceFromSystem = sqrt((this.position.x - this.system.position.x)*(this.position.x - this.system.position.x) + (this.position.y - this.system.position.y)*(this.position.y - this.system.position.y));
+    this.angle = atan2(this.system.position.y - this.position.y, this.system.position.x - this.position.x);
   
-  this.distanceFromSystem = sqrt((this.position.x - this.system.position.x)*(this.position.x - this.system.position.x) + (this.position.y - this.system.position.y)*(this.position.y - this.system.position.y));
-  this.angle = atan2(this.system.position.y - this.position.y, this.system.position.x - this.position.x);
+    this.velocity.x += cos(this.angle)*this.distanceFromSystem*this.distanceFromSystem*this.system.gravity/100000;
+    this.velocity.y += sin(this.angle)*this.distanceFromSystem*this.distanceFromSystem*this.system.gravity/100000;
   
-  this.velocity.x += cos(this.angle)*this.distanceFromSystem*this.distanceFromSystem*this.system.gravity/100000;
-  this.velocity.y += sin(this.angle)*this.distanceFromSystem*this.distanceFromSystem*this.system.gravity/100000;
-  
-  this.position.x += this.velocity.x;
-  this.position.y += this.velocity.y;
-  
-  
-  this.lifespan-=2;
-  
-  if (this.lifespan <= 1){
-    this.explode();
-  }
-  this.mass += 0.05;
+    this.previousPosition = createVector(this.position.x, this.position.y);
+    
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    
+    
+    
+    
+    this.mass += 0.05;
 };
 
 
 Particle.prototype.display = function() {
-  fill(this.distanceFromSystem, this.distanceFromSystem%3, this.distanceFromSystem%4);
-  stroke(this.distanceFromSystem%2);
-  ellipse(this.position.x, this.position.y, this.mass, this.mass);
+    if (this.system.trace == false){
+        fill(this.distanceFromSystem, this.distanceFromSystem%3, this.distanceFromSystem%4);
+        stroke(this.distanceFromSystem%2);
+        ellipse(this.position.x, this.position.y, this.mass, this.mass);
+    }
+    else{
+        strokeWeight(1);
+        stroke(255);
+        line(this.previousPosition.x, this.previousPosition.y, this.position.x, this.position.y)
+    }
+  
 };
 
 Particle.prototype.explode = function(){
-  ellipse(this.position.x, this.position.y, 10, 10);
+    this.system.
+    ellipse(this.position.x, this.position.y, 10, 10);
 };
