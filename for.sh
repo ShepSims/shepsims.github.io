7909d6ae1b03d20c8922b0372aed57c989c968e8
Shep Sims <shepsims@gmail.com>
2021-06-23 00:28:15 -0700

    add lines starting at 46 to commitbullit.txt

--- for.sh
+++ for.sh
@@ -1,7 +1,7 @@
 # Not liking the way your GitHub page looks because you aren't an active developer?  
 # Fix that quickly and easily with commit bullit
  
-echo    Commit Blaster 6000
+echo    Commit Blaster 9000
 echo  copyright Shep Sims 2021
 echo
 echo Make sure you have a git repo already initialized and linked to you current working directory
@@ -15,7 +15,7 @@ touch commitbullit.js
 for (( i = 1; i <= 100; i++ ))
 do
 	lineNumber=$(( $RANDOM % 50 ))
-	lineNumber2=$(($lineNumber+63))
+	lineNumber2=$(($lineNumber+ $RANDOM % 50))
 	echo commit number $i
 	sed -n $lineNumber,$lineNumber2'p' ./gravity/js/particle.js >> commitbullit.js # grabs random 
 	git add .
