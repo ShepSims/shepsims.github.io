a5608fdde9b1c154b7f2eb2953264dde63ebea15
Shep Sims <shepsims@gmail.com>
2021-06-23 00:00:36 -0700

    add lines starting at  to commitbullit.txt

--- for.sh
+++ for.sh
@@ -1,15 +1,22 @@
-#!/bin/bash
-START=1
-END=5
-echo "Commit Count:"
+# Not liking the way your GitHub page looks because you aren't an active developer?  
+# Fix that quickly and easily with commit bullit
+ 
+echo Commit Bullit
+echo copyright Shep Sims 2021
+echo
+echo Make sure you have a git repo already initialized and linked to the current directory
+touch commitbullit.txt
 
-for (( i = 1; i <= 6; i++ ))
+# get a random line number
+lineNumber = awk -v min=5 -v max=10 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'
+
+for (( i = 1; i <= 100; i++ ))
 do
-	echo $i
-	echo "hello world" >> txt.txt 
+	echo commit number $i
+	sed -n '$lineNumber,$lineNumber+5' ./gravity/js/particle.js >> commitbullit.txt
 	git add .
-	git commit -m "add hello world to txt.txt again"
-	sleep 1
+	git commit -m "add lines starting at $lineNumber to commitbullit.txt"
+	sleep 2
 done
 
 echo
