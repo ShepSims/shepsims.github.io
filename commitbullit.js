25d4bf8f31ff69c104567fb189cb429a913f913e
Shep Sims <shepsims@gmail.com>
2021-06-23 00:28:19 -0700

    add lines starting at 40 to commitbullit.txt

--- commitbullit.js
+++ commitbullit.js
@@ -550,3 +550,23 @@ Particle.prototype.display = function() {
 
     this.routeTable = { 
         portOne: false, 
+    this.angle = atan2(this.system.position.y - this.position.y, this.system.position.x - this.position.x);
+    
+    if (this.system.gravityType == true){
+        this.velocity.x += cos(this.angle)*this.distanceFromSystem*this.distanceFromSystem/100000;
+        this.velocity.y += sin(this.angle)*this.distanceFromSystem*this.distanceFromSystem/100000;
+    } else {
+        this.velocity.x += cos(this.angle)/(this.distanceFromSystem);
+        this.velocity.y += sin(this.angle)/(this.distanceFromSystem);
+    }
+    
+  
+    this.previousPosition = createVector(this.position.x, this.position.y);
+    
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    
+
+    this.mass += this.system.growthRate;
+    
+    if (this.mass <=0){this.mass = .1;}
