f41ba001b3029778f66bd0157be45467c0e9297b
Shep Sims <shepsims@gmail.com>
2021-06-23 00:28:21 -0700

    add lines starting at 45 to commitbullit.txt

--- commitbullit.js
+++ commitbullit.js
@@ -570,3 +570,14 @@ Particle.prototype.display = function() {
     this.mass += this.system.growthRate;
     
     if (this.mass <=0){this.mass = .1;}
+    } else {
+        this.velocity.x += cos(this.angle)/(this.distanceFromSystem);
+        this.velocity.y += sin(this.angle)/(this.distanceFromSystem);
+    }
+    
+  
+    this.previousPosition = createVector(this.position.x, this.position.y);
+    
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    
