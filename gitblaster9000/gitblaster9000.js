d09d753d7e782e584c5dcfe0b10d32223adf9b43
Shep Sims <shepsims@gmail.com>
2021-06-23 23:36:41 -0700

    copy lines 49 to 97 to gitblaster9000.js

--- gitblaster9000/gitblaster9000.js
+++ gitblaster9000/gitblaster9000.js
@@ -3542,3 +3542,52 @@ Particle.prototype.update = function() {
     if (this.system.gravityType == true){
     this.mass = 5;
   
+    
+  
+    this.previousPosition = createVector(this.position.x, this.position.y);
+    
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    
+
+    this.mass += this.system.growthRate;
+    
+    if (this.mass <=0){this.mass = .1;}
+
+    this.connected=false;
+    
+};
+
+
+Particle.prototype.display = function() {
+    stroke(color(r,g,b));
+    fill(color(r,g,b));
+    if (this.drawType == 'dot'){
+        ellipse(this.position.x, this.position.y, this.mass, this.mass);
+    }
+    else if(this.drawType == 'line'){
+        strokeWeight(1);
+        if (!this.system.sticks){
+            line(this.previousPosition.x, this.previousPosition.y, this.position.x, this.position.y);
+        }
+        else{
+            line(this.position.x, this.position.y, this.position.x+this.mass, this.position.y);
+        }
+    }
+  
+};
+
+Particle.prototype.getClosest = function() {
+    let closest;
+    let closest2;
+    let closest3;
+    let distance = 100000;
+    let distance2 = 100000;
+    let distance3 = 100000;
+
+    let i;
+    for (i = 0;i<this.system.particles.length-1;i++) {
+        particle = this.system.particles[i];
+        if (particle != this){
+            partdist = sqrt((this.position.x - particle.position.x)*(this.position.x - particle.position.x) + (this.position.y - particle.position.y)*(this.position.y - particle.position.y));
+            if (partdist<distance){
