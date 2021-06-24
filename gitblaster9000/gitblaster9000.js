9d1adcc1ff19c1fb626b19ac8647189e5e249acb
Shep Sims <shepsims@gmail.com>
2021-06-23 23:36:42 -0700

    copy lines 17 to 43 to gitblaster9000.js

--- gitblaster9000/gitblaster9000.js
+++ gitblaster9000/gitblaster9000.js
@@ -4020,3 +4020,30 @@ Particle.prototype.update = function() {
     
   
     this.previousPosition = createVector(this.position.x, this.position.y);
+    
+    this.velocity = createVector(0,0);
+    this.acceleration = createVector(0,0);
+
+  
+    this.distanceFromSystem = 0;
+    this.angle = 0;
+  
+    this.past = [];
+    this.lifespan = 255;
+    this.mass = 5;
+  
+};
+
+Particle.prototype.update = function() {
+    
+    this.previousDistance = this.distanceFromSystem;
+    this.wasGettingCloser = true;
+    this.connectCount = this.system.connectCount;
+    
+    
+    this.distanceFromSystem = sqrt((this.position.x - this.system.position.x)*(this.position.x - this.system.position.x) + (this.position.y - this.system.position.y)*(this.position.y - this.system.position.y));
+    
+    this.angle = atan2(this.system.position.y - this.position.y, this.system.position.x - this.position.x);
+    
+    if (this.system.gravityType == true){
+        this.velocity.x += cos(this.angle)*this.distanceFromSystem*this.distanceFromSystem/100000;
