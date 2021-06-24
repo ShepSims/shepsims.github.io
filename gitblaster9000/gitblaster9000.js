b7ca78ab2937b1f80ad35f1d927b420ae39c2d60
Shep Sims <shepsims@gmail.com>
2021-06-23 23:36:41 -0700

    copy lines 20 to 35 to gitblaster9000.js

--- gitblaster9000/gitblaster9000.js
+++ gitblaster9000/gitblaster9000.js
@@ -3333,3 +3333,19 @@ Particle.prototype.update = function() {
     
     this.previousDistance = this.distanceFromSystem;
     this.wasGettingCloser = true;
+
+  
+    this.distanceFromSystem = 0;
+    this.angle = 0;
+  
+    this.past = [];
+    this.lifespan = 255;
+    this.mass = 5;
+  
+};
+
+Particle.prototype.update = function() {
+    
+    this.previousDistance = this.distanceFromSystem;
+    this.wasGettingCloser = true;
+    this.connectCount = this.system.connectCount;
