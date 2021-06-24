077cee7fc8616344197b6ccdec3d04d114dc49f3
Shep Sims <shepsims@gmail.com>
2021-06-23 23:36:40 -0700

    copy lines 46 to 69 to gitblaster9000.js

--- gitblaster9000/gitblaster9000.js
+++ gitblaster9000/gitblaster9000.js
@@ -2208,3 +2208,27 @@ Particle.prototype.display = function() {
         ellipse(this.position.x, this.position.y, this.mass, this.mass);
     }
     else if(this.drawType == 'line'){
+        this.velocity.x += cos(this.angle)/(this.distanceFromSystem);
+        this.velocity.y += sin(this.angle)/(this.distanceFromSystem);
+    }
+    
+  
+    this.previousPosition = createVector(this.position.x, this.position.y);
+    
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    
+
+    this.mass += this.system.growthRate;
+    
+    if (this.mass <=0){this.mass = .1;}
+
+    this.connected=false;
+    
+};
+
+
+Particle.prototype.display = function() {
+    stroke(color(r,g,b));
+    fill(color(r,g,b));
+    if (this.drawType == 'dot'){
