51e0527470a101b9870dccb73aa665715a0b9e97
Shep Sims <shepsims@gmail.com>
2021-06-23 23:36:48 -0700

    copy lines 3 to 14 to gitblaster9000.js

--- gitblaster9000/gitblaster9000.js
+++ gitblaster9000/gitblaster9000.js
@@ -9519,3 +9519,15 @@ Particle.prototype.getClosest = function() {
     let closest;
     let closest2;
     let closest3;
+// ----------------------------------------
+
+let Particle = function(system) {
+    this.system = system;
+    this.drawType = this.system.drawType;
+    this.growthRate = this.system.growthRate;
+    this.connectType = "closest";
+
+    this.routeTable = { 
+        portOne: false, 
+        portTwo: false,
+        portThree: false };
