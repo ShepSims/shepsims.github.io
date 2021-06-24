f75cd8a03fa29e30cf92bf639c67e0203c9a1a6f
Shep Sims <shepsims@gmail.com>
2021-06-23 23:36:41 -0700

    copy lines 13 to 25 to gitblaster9000.js

--- gitblaster9000/gitblaster9000.js
+++ gitblaster9000/gitblaster9000.js
@@ -3650,3 +3650,16 @@ let Particle = function(system) {
     
 
     this.mass += this.system.growthRate;
+        portTwo: false,
+        portThree: false };
+
+    this.position = createVector(this.system.position.x + random(-50,50), this.system.position.y+ random(-50,50));
+    
+    this.velocity = createVector(0,0);
+    this.acceleration = createVector(0,0);
+
+  
+    this.distanceFromSystem = 0;
+    this.angle = 0;
+  
+    this.past = [];
