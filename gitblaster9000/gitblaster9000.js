20316e38ab7b2031d05018ad2a5b099196d7de71
Shep Sims <shepsims@gmail.com>
2021-06-23 23:36:40 -0700

    copy lines 2 to 22 to gitblaster9000.js

--- gitblaster9000/gitblaster9000.js
+++ gitblaster9000/gitblaster9000.js
@@ -2437,3 +2437,24 @@ let Particle = function(system) {
 Particle.prototype.update = function() {
     
     this.previousDistance = this.distanceFromSystem;
+//               PARTICLE
+// ----------------------------------------
+
+let Particle = function(system) {
+    this.system = system;
+    this.drawType = this.system.drawType;
+    this.growthRate = this.system.growthRate;
+    this.connectType = "closest";
+
+    this.routeTable = { 
+        portOne: false, 
+        portTwo: false,
+        portThree: false };
+
+    this.position = createVector(this.system.position.x + random(-50,50), this.system.position.y+ random(-50,50));
+    
+    this.velocity = createVector(0,0);
+    this.acceleration = createVector(0,0);
+
+  
+    this.distanceFromSystem = 0;
