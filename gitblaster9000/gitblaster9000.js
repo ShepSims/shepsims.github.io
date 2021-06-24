4bb642ecb59109ffbe12476cb1750672c6db5b80
Shep Sims <shepsims@gmail.com>
2021-06-23 23:36:41 -0700

    copy lines 12 to 29 to gitblaster9000.js

--- gitblaster9000/gitblaster9000.js
+++ gitblaster9000/gitblaster9000.js
@@ -3788,3 +3788,21 @@ Particle.prototype.update = function() {
     this.distanceFromSystem = sqrt((this.position.x - this.system.position.x)*(this.position.x - this.system.position.x) + (this.position.y - this.system.position.y)*(this.position.y - this.system.position.y));
     
     this.angle = atan2(this.system.position.y - this.position.y, this.system.position.x - this.position.x);
+        portOne: false, 
+        portTwo: false,
+        portThree: false };
+
+    this.position = createVector(this.system.position.x + random(-50,50), this.system.position.y+ random(-50,50));
+    
+    this.velocity = createVector(0,0);
+    this.acceleration = createVector(0,0);
+
+  
+    this.distanceFromSystem = 0;
+    this.angle = 0;
+  
+    this.past = [];
+    this.lifespan = 255;
+    this.mass = 5;
+  
+};
