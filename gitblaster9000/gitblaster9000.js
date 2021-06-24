0bc650faa3f3a6519b01d2c96001c6012ea39f27
Shep Sims <shepsims@gmail.com>
2021-06-23 23:36:46 -0700

    copy lines 43 to 59 to gitblaster9000.js

--- gitblaster9000/gitblaster9000.js
+++ gitblaster9000/gitblaster9000.js
@@ -7491,3 +7491,20 @@ let Particle = function(system) {
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     
+        this.velocity.x += cos(this.angle)*this.distanceFromSystem*this.distanceFromSystem/100000;
+        this.velocity.y += sin(this.angle)*this.distanceFromSystem*this.distanceFromSystem/100000;
+    } else {
+        this.velocity.x += cos(this.angle)/(this.distanceFromSystem);
+        this.velocity.y += sin(this.angle)/(this.distanceFromSystem);
+    }
+    
+  
+    this.previousPosition = createVector(this.position.x, this.position.y);
+    
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    
+
+    this.mass += this.system.growthRate;
+    
+    if (this.mass <=0){this.mass = .1;}
