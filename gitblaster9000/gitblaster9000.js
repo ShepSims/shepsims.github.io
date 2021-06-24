e0e0b96e4685dfad547b2e257c6345b77d8168f0
Shep Sims <shepsims@gmail.com>
2021-06-23 23:36:40 -0700

    copy lines 43 to 49 to gitblaster9000.js

--- gitblaster9000/gitblaster9000.js
+++ gitblaster9000/gitblaster9000.js
@@ -2822,3 +2822,10 @@ Particle.prototype.update = function() {
     }
     
   
+        this.velocity.x += cos(this.angle)*this.distanceFromSystem*this.distanceFromSystem/100000;
+        this.velocity.y += sin(this.angle)*this.distanceFromSystem*this.distanceFromSystem/100000;
+    } else {
+        this.velocity.x += cos(this.angle)/(this.distanceFromSystem);
+        this.velocity.y += sin(this.angle)/(this.distanceFromSystem);
+    }
+    
