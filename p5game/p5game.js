e8bc4ca9fd808b486fb34b898025a73859a1ccb4
Shep Sims <shepsims@gmail.com>
2024-11-25 11:00:49 -0500

    Add projectiles, ammo pickup, enhanced design

--- p5game/p5game.js
+++ p5game/p5game.js
@@ -1,17 +1,3 @@
-// function preload() {
-// 	var banner = new Image();
-// 	var loading = new Image();
-// 	var bannerElement = document.getElementById('banner'); // assumes an element with id "banner" contains the banner image - you can get the element however you want.
-// 	banner.src = './goose.gif';
-// 	loading.src = 'loading.gif';
-// 	banner.onload = function () {
-// 		bannerElement.removeChild(bannerElement.lastChild);
-// 		bannerElement.appendChild(banner);
-// 	};
-// 	bannerElement.removeChild(bannerElement.lastChild);
-// 	bannerElement.appendChild(loading);
-// }
-
 function setup() {
 	canvas = createCanvas(windowWidth - 50, windowHeight - 50);
 
@@ -21,7 +7,7 @@ function setup() {
 	rectMode(CORNERS);
 	door = [windowWidth - 130, windowHeight - 750, windowWidth - 75, windowHeight - 850];
 	doorknob = [windowWidth - 120, windowHeight - 800];
-	tramp = [350, windowHeight - 450, 600, windowHeight - 475];
+	trampoline = [350, windowHeight - 450, 600, windowHeight - 475];
 	angleMode(DEGREES);
 	angle = 0;
 	x = 0;
@@ -31,6 +17,8 @@ function setup() {
 }
 
 function draw() {
+	background(255);
+
 	onLine =
 		getDistance(lineCoords[0], lineCoords[1], character.position.x, character.position.y) +
 			getDistance(lineCoords[2], lineCoords[3], character.position.x, character.position.y) <=
@@ -55,11 +43,11 @@ function draw() {
 		character.velocity.y = 0;
 	}
 	if (keyIsDown(UP_ARROW)) {
-		if (inDoor(door[0], door[1], door[2], door[3], character.position.x, character.position.y)) {
+		if (inContact(door[0], door[1], door[2], door[3], character.position.x, character.position.y)) {
 			// window.location.href = '../gravity/html/gravity.html';
 			won = true;
 		}
-		if (inDoor(tramp[0], tramp[1], tramp[2], tramp[3], character.position.x, character.position.y)) {
+		if (inContact(trampoline[0], trampoline[1], trampoline[2], trampoline[3], character.position.x, character.position.y)) {
 			character.velocity.y = -Math.abs(character.velocity.y) - 2;
 		} else if (character.position.y != windowHeight - 50 && !onLine) {
 			character.velocity.y -= 0.2;
@@ -75,27 +63,31 @@ function draw() {
 	}
 
 	if (keyIsDown(65)) {
-		a = { x: -1, y: 0 };
+		// 'A' key
+		a = { x: -3, y: 0 };
 		if (character.shotClock < 1) {
-			fire(a);
+			character.fire({ ...a, life: 1000 });
 		}
 	}
 	if (keyIsDown(87)) {
-		a = { x: -1, y: 0 };
+		// 'W' key
+		a = { x: -3, y: 0 };
 		if (character.shotClock < 1) {
-			fire(a);
+			character.fire({ ...a, life: 1000 });
 		}
 	}
 	if (keyIsDown(83)) {
-		a = { x: 0, y: -1 };
+		// 'S' key
+		a = { x: 0, y: -3 };
 		if (character.shotClock < 1) {
-			fire(a);
+			character.fire({ ...a, life: 1000 });
 		}
 	}
 	if (keyIsDown(68)) {
-		a = { x: 1, y: 0 };
+		// 'D' key
+		a = { x: 3, y: 0 };
 		if (character.shotClock < 1) {
-			fire(a);
+			character.fire({ ...a, life: 1000 });
 		}
 	}
 
@@ -105,9 +97,48 @@ function draw() {
 		character.position.y = lineCoords[1];
 	}
 
+	// Draw the door
+	fill(255, 0, 0); // Change color to red
+
 	rect(door[0], door[1], door[2], door[3]);
+	fill(0, 0, 0); // Change color to red
+
 	circle(doorknob[0], doorknob[1], 5);
-	rect(tramp[0], tramp[1], tramp[2], tramp[3]);
+	rect(trampoline[0], trampoline[1], trampoline[2], trampoline[3]);
+
+	// Define the ammo pickup
+	let ammoPickup = {
+		x: (3 * windowWidth) / 4, // X position of the ammo pickup (adjusted to be within the screen)
+		y: windowHeight - 200, // Y position of the ammo pickup (adjusted to be within the screen)
+		size: 25, // Size of the ammo pickup
+		visible: true, // Track visibility of the ammo pickup
+	};
+
+	// Draw the ammo pickup
+	if (ammoPickup.visible) {
+		fill(0, 0, 255); // Change color to blue
+		rectMode(CENTER);
+		rect(ammoPickup.x, ammoPickup.y, ammoPickup.size, ammoPickup.size); // Ensure width and height are both 25
+		rectMode(CORNERS);
+		fill(0, 0, 0); // Change color to blue
+	}
+
+	// Check for collision with the ammo pickup
+	if (
+		character.position.x < ammoPickup.x + ammoPickup.size &&
+		character.position.x + 50 > ammoPickup.x && // Assuming character width is 50
+		character.position.y < ammoPickup.y + ammoPickup.size &&
+		character.position.y + 50 > ammoPickup.y
+	) {
+		// Assuming character height is 50
+		character.ammo += 5; // Refill ammo (adjust the amount as needed)
+		ammoPickup.visible = false; // Make the pickup disappear
+
+		// Regenerate the ammo pickup after 2.5 seconds
+		setTimeout(() => {
+			ammoPickup.visible = true; // Make the pickup visible again
+		}, 2500); // 2500 milliseconds = 2.5 seconds
+	}
 
 	character.position.y += character.velocity.y;
 	renderCharacter(character.position.x, character.position.y);
@@ -118,6 +149,34 @@ function draw() {
 	} else {
 		text('YOU WIN!', windowWidth / 2 - 100, (windowHeight - 100) / 2);
 	}
+
+	// Display the ammo counter
+	fill(0); // Set the color for the text (black)
+	textSize(20); // Set the text size
+	text(`Ammo: ${character.ammo}`, 10, 30); // Display the ammo count at (10, 30)
+
+	// Draw the projectiles
+	for (let i = 0; i < character.lasers.length; i++) {
+		let laser = character.lasers[i];
+		fill(255, 0, 0); // Color of the projectile (red)
+		ellipse(laser.x, laser.y, 5, 5); // Draw the projectile as a small circle
+
+		// Update the position of the laser
+		laser.x += laser.xv;
+		laser.y += laser.yv;
+
+		// Decrease the life of the laser
+		laser.life--;
+
+		// Remove the laser if its life is over
+		if (laser.life <= 0) {
+			character.lasers.splice(i, 1);
+			i--; // Adjust index after removal
+		}
+	}
+
+	// Reset fill color for other drawings
+	fill(0); // Reset to black or any other color you want for other elements
 }
 
 function getDistance(xA, yA, xB, yB) {
@@ -127,7 +186,7 @@ function getDistance(xA, yA, xB, yB) {
 	return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
 }
 
-function inDoor(dx1, dy1, dx2, dy2, px, py) {
+function inContact(dx1, dy1, dx2, dy2, px, py) {
 	if (px > dx1 && px < dx2 && py < dy1 && py > dy2) {
 		return true;
 	}
